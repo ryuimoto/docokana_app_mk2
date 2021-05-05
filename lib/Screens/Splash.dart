975760abeeed2dfe7aaab59ad7558ee00a59d3d1
ryuimoto <>
2021-05-05 15:24:23 +0900

    device_id add

--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:docokana_new_app_mk2/Screens/Tab.dart';
 import 'package:docokana_new_app_mk2/Screens/auth/login.dart';
+import 'package:http/http.dart';
+import 'package:docokana_new_app_mk2/api/url.dart';
+import 'package:device_info/device_info.dart';
+import 'dart:io';
+import 'dart:convert';
+
 
 class Splash extends StatefulWidget {
   @override
@@ -22,6 +28,7 @@ class _SplashState extends State<Splash> {
 
   @override
   Widget build(BuildContext context) {
+    loginState();
     return Scaffold(
         backgroundColor: Colors.white,
         body: Center(
@@ -33,5 +40,34 @@ class _SplashState extends State<Splash> {
                 fit: BoxFit.contain,
               )),
         ));
-  }
+    }
+
+    void loginState() async {
+      String deviceId = '';
+
+      if(Platform.isAndroid) {
+        print('android!!');
+        DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+        deviceId = '${androidInfo.androidId}';
+
+      } else if(Platform.isIOS) {
+        print('ios!!');
+        DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
+        deviceId = '${iosDeviceInfo.identifierForVendor}';
+
+      }
+
+      print(deviceId);
+
+      String url = Url().url + 'get_user';
+      Map<String, String> headers = {'content-type': 'application/json'};
+      String body = json.encode({
+        'device_id': deviceId,
+      });
+
+      print(body);
+
+    }
 }
